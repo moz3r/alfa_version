1938961d062a9c4279810d357002ec81d5071187
Akos <Akos@SID-5896.local>
2019-04-17 16:58:17 +0100

    latest

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,19 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
+  
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className='Hero'>
+      <div className='HeroGroup'>
+        <h1>Fisrt attempt</h1>
+        <p>Random text</p>
+        <p>Batman2.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+
+      </div>
+    </div>
   </div>
 )
+
 
 export default IndexPage
